3131b2e18cea971e02837b4befc54c16b6264eed
Daniel Barbosa <danielbarbosa.p@outlook.com>
2021-03-14 17:09:05 -0300

    [Version 1.0.0] Creating projects templates and shared services"

---
+++ QRCode.Application/Services/CodeGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using System.IO;
+using QRCoder;
+
+namespace QRCode.Application.Services
+{
+    public static class CodeGenerator
+    {
+        public static byte[] GenerateByteArray(string url)
+        {
+            var image = GenerateCodeImage(url);
+            return ImageToBytes(image);
+        }
+        private static byte[] ImageToBytes(Image image)
+        {
+            using var stream = new MemoryStream();
+            image.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+            return stream.ToArray();
+            
+        }
+        public static Bitmap GenerateCodeImage(string url)
+        {
+            var qrCodeGenerator = new QRCodeGenerator();
+            var qrCodeData = qrCodeGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCode(qrCodeData);
+            var qrCodeImage = qrCode.GetGraphic(10);
+            return qrCodeImage;
+        }
+    }
+}
